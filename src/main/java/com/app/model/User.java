fc68852ec0fb53c4f657f6782f31aa972151c750
manesunil <manesunil069@gmail.com>
2023-04-16 09:15:35 +0530

    1992-1 Add cotroller

---
+++ src/main/java/com/app/model/User.java
@@ -0,0 +1,22 @@
+package com.app.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+@Getter
+@Setter
+@ToString
+public class User {
+    private Integer userId;
+    private String email;
+    private String pass;
+    private Integer gender;
+    private Integer age;
+    private String roles;
+
+
+
+
+
+}
